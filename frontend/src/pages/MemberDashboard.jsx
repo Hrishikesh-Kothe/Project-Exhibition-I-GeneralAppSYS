a5c529b3757e79bf0a32dbbee9c9f53db40c21a9
Hrishikesh Kothe <hrishikothe@gmail.com>
2025-09-13 03:08:39 +0530

    Re-add frontend as normal folder (fix submodule issue)

---
+++ frontend/src/pages/MemberDashboard.jsx
@@ -0,0 +1,13 @@
+// frontend/src/pages/MemberDashboard.jsx
+import "./MemberDashboard.css";  // link CSS
+
+export default function MemberDashboard() {
+  return (
+    <div className="Main-Dash">
+    <div className="dashboard">
+      <h1 className="Welhead">Welcome Back, Member</h1>
+      <h1 className="Selhead">Select your service</h1>
+    </div>
+    </div>
+  );
+}
---
+++ frontend/src/pages/SigninMember.jsx
@@ -0,0 +1,26 @@
+import "./SigninMember.css";
+
+export default function SigninMember() {
+  return (
+    <div className="signin-page">
+      <div className="signin-box">
+        <h2 className="signin-title">Welcome Back</h2>
+
+        <button className="signin-btn">Sign In</button>
+
+        <form className="signin-form">
+          <label>Username</label>
+          <input type="text" placeholder="Enter your username" />
+
+          <label>Password</label>
+          <input type="password" placeholder="Enter your password" />
+
+          <p className="link">Specialist Sign In</p>
+          <p className="link">Not a member? Register</p>
+        </form>
+
+        <button className="signin-btn">Continue</button>
+      </div>
+    </div>
+  );
+}
